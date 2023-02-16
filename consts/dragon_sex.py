28d8c71499a57284c19b2d226621246f82c7d960
Mikhail Krotov <genry.foer@gmail.com>
2023-02-16 14:18:15 +0600

    sex fix

--- consts/dragon_sex.py
+++ consts/dragon_sex.py
@@ -1,3 +1,3 @@
 dragon_sex = ['female', 'male', 'unknown']
 
-dragons_sex_titles = { 'female': 'Самец', 'male': 'Самка', 'unknown': 'Не известен' }
+dragons_sex_titles = { 'male': 'Самец', 'female': 'Самка', 'unknown': 'Не известен' }
